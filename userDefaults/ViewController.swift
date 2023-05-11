e989f91484fba4a9a2c1737c22522047898134d0
Deepthi012 <130980899+Deepthi012@users.noreply.github.com>
2023-05-11 16:56:02 +0530

    Todolist app using user defaults

---
+++ userDefaults/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  userDefaults
+//
+//  Created by West Agile Labs on 19/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        retriveDetails()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveButton(_ sender: UIButton) {
+        UserDefaults.standard.set(nameText.text, forKey: "Name" )
+        UserDefaults.standard.set(emailText.text, forKey: "Email")
+        UserDefaults.standard.set(passwordText.text, forKey: "Password" )
+    }
+    func retriveDetails(){
+        let name = UserDefaults.standard.string(forKey: "Name" )
+        let email = UserDefaults.standard.string(forKey: "Email" )
+        let password = UserDefaults.standard.string(forKey: "Password")
+        print("your name is \(name!), your email is \(email!), your password is \(password!)")
+    }
+    
+}
+
