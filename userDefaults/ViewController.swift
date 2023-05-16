3f7d43f735f1daae99f7bf46feb12aef31999ac5
Deepthi012 <130980899+Deepthi012@users.noreply.github.com>
2023-05-16 20:11:53 +0530

    added latest changes

--- userDefaults/ViewController.swift
+++ userDefaults/ViewController.swift
@@ -14,23 +14,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     
     @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        retriveDetails()
+        //retriveDetails()
         // Do any additional setup after loading the view.
     }
-
+    
+    
     @IBAction func saveButton(_ sender: UIButton) {
+        
+        
         UserDefaults.standard.set(nameText.text, forKey: "Name" )
         UserDefaults.standard.set(emailText.text, forKey: "Email")
         UserDefaults.standard.set(passwordText.text, forKey: "Password" )
+        retriveDetails()
+        
     }
+    
+    
+    
     func retriveDetails(){
+        
         let name = UserDefaults.standard.string(forKey: "Name" )
         let email = UserDefaults.standard.string(forKey: "Email" )
         let password = UserDefaults.standard.string(forKey: "Password")
-        print("your name is \(name!), your email is \(email!), your password is \(password!)")
+        
+       print("Your name is \(name!), your email is \(email!), your password is \(password!)")
     }
-    
+   
 }
+
 
